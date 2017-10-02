adf03eecca23bb44d029a923e7ec823aab172d5e
rafael-at97 <31388822+rafael-at97@users.noreply.github.com>
2017-10-02 13:01:25 -0300

    Update MotorPasso.ino

--- MotorPasso.ino
+++ MotorPasso.ino
@@ -5,6 +5,13 @@ int IN2 = 9;
 int IN3 = 10;
 int IN4 = 11;
 
+/*Tabela de sequência para os passos*/
+bool sequence[4][4] = {   {1, 0, 0, 1},
+                          {0, 1, 0, 1},
+                          {0, 1, 1, 0},
+                          {1, 0, 1, 0}
+ };
+
 void setup(){
   // Define os pinos como saida
   pinMode(IN1,OUTPUT);
@@ -15,32 +22,19 @@ void setup(){
 
 int tempo = 10;
 
+void rotate(int &state, int steps){
+    for(int i = 0; i < abs(steps); i++){
+        state = (state + steps/abs(steps) + 4)%4;
+        digitalWrite(IN1, sequence[state][0]);
+        digitalWrite(IN2, sequence[state][1]);
+        digitalWrite(IN3, sequence[state][2]);
+        digitalWrite(IN4, sequence[state][3]);
+    };
+    delay(tempo);
+}
+
 void loop(){
-  // Passo 1
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);  
-
-  // Passo 2
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 0);
-  digitalWrite(IN4, 1);
-  delay(tempo);
-
-  // Passo 3
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 1);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
-
-  // Passo 4
-  digitalWrite(IN1, 1);
-  digitalWrite(IN2, 0);
-  digitalWrite(IN3, 1);
-  digitalWrite(IN4, 0);
-  delay(tempo);
+  int state = 0;
+  
+  rotate(state, 5);
 }
