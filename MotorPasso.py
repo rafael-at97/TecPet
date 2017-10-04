176877fedeb9c319992b80ba02b2b1b09a1e7c07
rafael-at97 <31388822+rafael-at97@users.noreply.github.com>
2017-10-04 09:17:32 -0300

    Update MotorPasso.py

--- MotorPasso.py
+++ MotorPasso.py
@@ -1,40 +1,51 @@
-// Conexões do "primeiro" motor
-int IN1 = 8;
-int IN2 = 9;
-// Conexões do "segundo" motor
-int IN3 = 10;
-int IN4 = 11;
+import time
+import os
+import RPi.GPIO as GPIO
+import numpy as np
 
-/*Tabela de sequência para os passos*/
-bool sequence[4][4] = {   {1, 0, 0, 1},
-                          {0, 1, 0, 1},
-                          {0, 1, 1, 0},
-                          {1, 0, 1, 0}
- };
+# Conexões do "primeiro" motor
+IN1 = 
+IN2 = 
+# Conexões do "segundo" motor
+IN3 = 
+IN4 = 
 
-void setup(){
-  // Define os pinos como saida
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-}
+# Tabela de sequência para os passos
+sequence = np.array([[1,0,0,1], [0,1,0,1], [0,1,1,0], [1, 0, 1, 0]])
 
-int tempo = 10;
+tempo = 10
 
-void rotate(int &state, int steps){
-    for(int i = 0; i < abs(steps); i++){
-        state = (state + steps/abs(steps) + 4)%4;
-        digitalWrite(IN1, sequence[state][0]);
-        digitalWrite(IN2, sequence[state][1]);
-        digitalWrite(IN3, sequence[state][2]);
-        digitalWrite(IN4, sequence[state][3]);
-    };
-    delay(tempo);
-}
-
-void loop(){
-  int state = 0;
+def gpio_setup():
+  # Define os pinos como saida
+  GPIO.setmode(GPIO.BCM)
   
-  rotate(state, 5);
-}
+  GPIO.setup(IN1, GPIO.OUT)
+  GPIO.setup(IN2, GPIO.OUT)
+  GPIO.setup(IN3, GPIO.OUT)
+  GPIO.setup(IN4, GPIO.OUT)
+
+def rotate(state, steps):
+    for i in range(0, abs(steps)):
+        state = (state + steps/abs(steps) + 4)%4
+        GPIO.output(IN1, sequence[state, 0])
+        GPIO.output(IN2, sequence[state, 1])
+        GPIO.output(IN3, sequence[state, 2])
+        GPIO.output(IN4, sequence[state, 3])
+    time.sleep(tempo)
+      
+    return state
+
+def main():
+  gpio_setup()
+  state = 0
+  
+  while True:
+      steps = input('Number of steps? (Can be negative)')
+      state = rotate(state, steps)
+
+try:
+    main()
+finally:
+    GPIO.cleanup()
+    print("Fim de Programa!")
+#END  
