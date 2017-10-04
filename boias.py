18057a761a7b6c03cfe4db4ea60e63706f084d08
rafael-at97 <31388822+rafael-at97@users.noreply.github.com>
2017-10-04 08:15:33 -0300

    Update boias.py

--- boias.py
+++ boias.py
@@ -12,18 +12,20 @@ def gpio_setup():
     # GPIO.BCM   = Número de "Broadcom SOC Channel", escritos depois de "GPIO" no esquemático da placa
     GPIO.setmode(GPIO.BCM)
     # Definição das boias como normalmente abertas
-    GPIO.setup (Boia_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup (Boia_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(Boia_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(Boia_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def main ():
     gpio_setup()
     while True:
-        boia_1=GPIO.input(Boia_1)
-        boia_2=GPIO.input(Boia_2)
+        boia_1 = GPIO.input(Boia_1)
+        boia_2 = GPIO.input(Boia_2)
         if boia_1 and boia_2:
         	# Tanque cheio, definir rotina:
         elif not boia_1 and boia_2:
             # Tanque pela "metade", definir rotina:
+        elif boia_1 and not boia_2:
+            # Erro nas leituras das boias:
         else:
         	# Nível de água abaixo da boia inferior, "vazio", definir rotina:    
         time.sleep(0.1)
@@ -32,5 +34,5 @@ try:
     main()
 finally:
     GPIO.cleanup()
-    print ("Fim de Programa!")
+    print("Fim de Programa!")
     #END
